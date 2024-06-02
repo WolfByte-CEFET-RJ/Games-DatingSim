91627d4fd39b5e7286b3bc83d881c459cd6d66a2
Caio Rodrigues <95007652+KaioshinRodrigues@users.noreply.github.com>
2024-06-01 21:13:23 -0300

    Menu de Pause e outros

--- Assets/Scripts/Core/GameManager.cs
+++ Assets/Scripts/Core/GameManager.cs
@@ -8,10 +8,14 @@ public class GameManager : MonoBehaviour
     // Instancia singular
     public static GameManager Instance;
 
+    // Informações do jogador
+    public string playerName;
+    public int affectionPoints;
+
     // Gênero selecionado
     public string selectedGender;
 
-    // Caixa de dialogo selecionada
+    // Caixa de diálogo selecionada
     public GameObject selectedDialogueBox;
 
     // Sprite para personagem que deve receber um gênero 
@@ -31,10 +35,36 @@ public class GameManager : MonoBehaviour
         }
     }
 
-     public void LoadNextScene(string NextScene)
+    public void LoadNextScene(string NextScene)
     {
         // Carrega a próxima cena
         SceneManager.LoadScene(NextScene);
     }
-}
 
+    // Método para salvar informações do jogador
+    public void SavePlayerData(string name, int points)
+    {
+        playerName = name;
+        affectionPoints = points;
+        // Você pode adicionar aqui outros códigos para salvar essas informações
+    }
+
+    // Método para carregar informações do jogador
+    public void LoadPlayerData()
+    {
+        // Você pode adicionar aqui o código para carregar as informações salvas, como de PlayerPrefs ou um arquivo JSON
+        // E atribuir os valores carregados às variáveis playerName e affectionPoints
+        
+        // Carregar o nome do jogador de PlayerPrefs, se existir
+        if (PlayerPrefs.HasKey("PlayerName"))
+        {
+            playerName = PlayerPrefs.GetString("PlayerName");
+        }
+
+        // Carregar os pontos de afetividade de PlayerPrefs, se existirem
+        if (PlayerPrefs.HasKey("AffectionPoints"))
+        {
+            affectionPoints = PlayerPrefs.GetInt("AffectionPoints");
+        }
+    }
+}
---
+++ Assets/Scripts/Core/PauseMenu.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject pauseMenu;
+    public static bool isPaused;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pauseMenu.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(isPaused)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
+    }
+
+    public void PauseGame()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+
+    public void ResumeGame()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+/*
+    public void GoToMainMenu()
+    {
+        Time.timeScale = 1f;
+        //Usar quando existir
+        //SceneManager.LoadScene("MainMenu");
+        isPaused = false;
+    }
+*/
+    public void ConfigGame()
+    {
+        isPaused = true;
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
