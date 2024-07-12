3fcdcaf27116669c26d3d7b37d494e63278dfa03
Caio Rodrigues <95007652+KaioshinRodrigues@users.noreply.github.com>
2024-07-12 17:42:22 -0300

    Troca de texto no JSON
    Muda pronome e nome em linhas de texto no JSON

--- Assets/Scripts/Core/JSONReader.cs
+++ Assets/Scripts/Core/JSONReader.cs
@@ -22,14 +22,39 @@ public class JSONReader : MonoBehaviour
     }
     public ListaFalas lista_de_falas = new ListaFalas();
     // Start is called before the first frame update
+
     void Start()
     {
         lista_de_falas = JsonUtility.FromJson<ListaFalas>(textJSON.text);
+        string playerName = GameManager.Instance.playerName;
+        string pronome = ObterPronome(GameManager.Instance.selectedGender);
+
+         foreach (Fala fala in lista_de_falas.fala)
+        {
+            // Substitui o nome do jogador
+            fala.dialogo = fala.dialogo.Replace("[NOME]", playerName);
+
+            // Substitui o pronome baseado no gênero selecionado
+            fala.dialogo = fala.dialogo.Replace("[PRONOME]", pronome);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private string ObterPronome(string pronome)
+    {
+        switch(pronome)
+        {
+            case "Male":
+                return "o";
+            case "Female":
+                return "a";
+            default:
+                return "o/a"; // Pronome genérico, você pode ajustar conforme necessário
+        }
     }
 }
