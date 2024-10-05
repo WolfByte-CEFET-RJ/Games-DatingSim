7113fb2c3b389720d69783392ffae8aa1795227e
FredericoSRamos <angysplays@gmail.com>
2024-10-05 00:34:34 -0300

    UI_EscolhaLocal

---
+++ Assets/Scripts/Core/ButoonScripts/InfoOnHover.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class InfoOnHover : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public CanvasGroup info;
+    bool fadeIn = false;
+    bool fadeOut = false;
+
+    void Awake()
+    {
+        info.alpha = 0;
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        fadeOut = false;
+        fadeIn = true;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        fadeIn = false;
+        fadeOut = true;
+    }
+
+    void Update()
+    {
+        if (fadeIn)
+        {
+            info.alpha += Time.deltaTime * 4;
+
+            if (info.alpha >= 1)
+                fadeIn = false;
+        }
+        else if (fadeOut)
+        {
+            info.alpha -= Time.deltaTime * 4;
+            
+            if (info.alpha <= 0)
+                fadeOut = false;
+        }
+    }
+}
